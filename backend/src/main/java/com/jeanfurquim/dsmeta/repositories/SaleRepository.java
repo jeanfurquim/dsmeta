455facbb032f531fcdc4ee8c1420b6a70292b2d9
jeanfurquim <jean.furquim@jpisistemas.com.br>
2022-07-14 15:13:19 -0300

    API test

---
+++ backend/src/main/java/com/jeanfurquim/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.jeanfurquim.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jeanfurquim.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
