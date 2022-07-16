5c0ece9c6933054df31c34b107cae0c52cf65813
jeanfurquim <jean.furquim@jpisistemas.com.br>
2022-07-16 19:19:21 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,7 +2,8 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from "components/NotificationButton";
 import "./styles.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function SalesCard() {
   
@@ -11,6 +12,13 @@ function SalesCard() {
 
   const [minDate, setMinDate] = useState(new Date(min));
   const [maxDate, setMaxDate] = useState(max);
+
+  useEffect(()=>{
+    axios.get("http://localhost:8080/sales")
+    .then(response=>{
+      console.log(response.data)
+    })
+  },[]);
 
 
 
